3501dd548ab887683728cebf839fb57da658b671
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-01-28 20:10:34 -0300

    Documenta a classe do módulo models

--- src/models/filtro.py
+++ src/models/filtro.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 @dataclass
 class Filtro:
+    """
+    Classe para representar um filtro
+    Args:
+        coluna: Coluna a ser filtrada
+        dataframe: DataFrame a ser filtrado
+        valor: Valor a ser filtrado
+    """
+    
     coluna: str
     dataframe: pd.DataFrame
     valor: str
